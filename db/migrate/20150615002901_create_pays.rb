d5f8ca1dddec0744ff78c623f78aeb6537e79795
Elton Silva <elton.chrls@gmail.com>
2015-09-14 16:03:37 -0300

    ok

--- db/migrate/20150615002901_create_pays.rb
+++ db/migrate/20150615002901_create_pays.rb
@@ -2,7 +2,7 @@ class CreatePays < ActiveRecord::Migration
   def change
     create_table :pays do |t|
       t.references :bill_category, index: true#, foreign_key: true
-      t.references :project, index: true,# foreign_key: true
+      t.references :project, index: true# foreign_key: true
       t.string :name
       t.text :description
       t.boolean :status, default: false
