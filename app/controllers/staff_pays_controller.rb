392bc10dfc9b2f61d5c4593070503aee43d64c98
Elton Silva <elton.chrls@gmail.com>
2015-11-03 15:47:59 -0200

    ok

--- app/controllers/staff_pays_controller.rb
+++ app/controllers/staff_pays_controller.rb
@@ -2,7 +2,7 @@ class StaffPaysController < ApplicationController
   before_action :set_project
 
   def index
-    @staff_pays = Pay.where(staff_id: current_user.id)
+    @staff_pays = @project.project_finances.where(staff_id: current_user.id)
   end
   
   private
